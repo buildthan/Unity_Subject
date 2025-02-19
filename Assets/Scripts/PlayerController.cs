3419f1fe8d38443079b5a88fef60afdc1f1edfe5
buildthan <a9175226@gmail.com>
2025-02-20 05:21:13 +0900

    Feat : 캐릭터 움직임 기능 구현
    Feat : 캐릭터 움직임 기능 구현
    
    캐릭터가 wasd로 움직입니다.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    Vector2 movement = Vector2.zero;
+    Vector2 direction = Vector2.zero;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(horizontal, vertical).normalized;
+        movement = movement * 5;
+        _rigidbody.velocity = movement; //키보드를 누른 방향으로 일정하게 움직임
+
+        if(horizontal > 0) //오른쪽을 향한 경우
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if(horizontal<0) //왼쪽을 향한 경우
+        {
+            _spriteRenderer.flipX = true;
+        }
+
+    }
+}
